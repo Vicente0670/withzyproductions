26607148f7442b061f969de4a2e8d9bca4861a19
Vicente0670 <mauras374@gmail.com>
2025-01-04 17:19:44 -0600

    🥚 Markup of Top of Main Page Completed

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,9 +1,17 @@
 import header from "./header.module.css";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <nav className={header.headerBase}>
-      
+      <a href="/" className={header.logo}/>
+      <div className={header.linkContainer}>
+        <Link href="/">Home</Link>
+        <Link href="/">Contact</Link>
+        <Link href="/">Portfolio</Link>
+        <Link href="/">Rates</Link>
+      </div>
+      <div className={header.headerBackground}></div>
     </nav>
   )
 }
