38fca60d451a2b946daa785bf41ac00bb098cc5e
Vicente0670 <122196085+Vicente0670@users.noreply.github.com>
2025-05-05 11:26:49 -0500

    🔩 Menu Link Addition (Fixes Needed)

--- src/components/header/menuFunction.tsx
+++ src/components/header/menuFunction.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 
 export default function MenuFunction() {
   const [isOpen, clicked] = useState(false);
-  
+
   function buttonClickEvent() {
     if (isOpen == true) {
       clicked(false);
@@ -17,19 +17,41 @@ export default function MenuFunction() {
     }
   }
 
+  function abortMenu() {
+    clicked(false);
+    document.documentElement.style.overflowY = "";
+  }
+
   return (
     <>
-      <div className="menuButtonContainer">
-        <div role="button" tabIndex={2} aria-expanded={isOpen} onClick={buttonClickEvent} className={`menuButton ${isOpen ? "open" : "closed"}`}>
+      <div className={`menuButtonContainer ${isOpen ? "open" : "closed"}`}>
+        <div role="button" tabIndex={0} aria-expanded={isOpen} onClick={buttonClickEvent} className={`menuButton ${isOpen ? "open" : "closed"}`}>
           <div className="bun"></div>
           <div className="meat"></div>
           <div className="bun"></div>
           <div className="meat"></div>
         </div>
-        <div className={`menuModal ${isOpen ? "open" : "closed"}`}>
-          <h2>This is some testing.</h2>
-          <p>This is a test of how long the essay is going to be for this specific project. I didn't know that I could write this long, just that my fingers are getting moren tired than a buffalo at sunset. By the way, the aforementioned joke does not make any sense at all in the slightest.</p>
+        <div className={`menuModalContainer ${isOpen ? "open" : "closed"}`} id="menuModalContainer">
+          <div className="menuModal">
+            <h2>Menu</h2>
+            <a href="/">Home</a>
+            <a href="/">Project 1</a>
+            <details>
+              <summary><p>Portfolio</p></summary>
+              <a href="/">Resume</a>
+              <a href="/">On Set With ZY</a>
+              <a href="/">Shoots With ZY</a>
+              <a href="/">Talks With ZY</a>
+            </details>
+            <a href="/">Contact</a>
+            <a href="/">Rates</a>
+            <a href="/">Project 2</a>
+            <a href="/">Project 3</a>
+            <a href="/">Project 4</a>
+            <a href="/">Project 5</a>
+            </div>
         </div>
+        <div className={`menuModalBackground ${isOpen ? "open" : "closed"}`} onClick={abortMenu}/>
         <div className="menuButtonWrapper">
           <div className={`menuButtonBackground ${isOpen ? "open" : "closed"}`}/>
         </div>
