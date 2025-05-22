dc4d100c6c6445731795797485035a6a9e9fd990
Vicente0670 <122196085+Vicente0670@users.noreply.github.com>
2025-05-21 22:47:48 -0500

    🥾 Implemented Footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -9,6 +9,9 @@ export default function Footer() {
     "Your bread and butter are the shutter speed, aperture, and ISO.",
     "What is this?",
     "That ain't me.",
+    "Are you sure this is going to work?",
+    "Curly fries are not my jam (I'm lying.)",
+    "Look at Mr. Boliminary. So... thrilling in his existence.",
   ];
 
   const key = Math.floor(Math.random() * quote.length);
@@ -16,20 +19,47 @@ export default function Footer() {
   return (
     <footer className="footer" role="contentinfo">
       <div className="logoContainer">
-        <div className="logo"/>
-        <Link href="/">With <span>Zy</span> Productions</Link>
+        <div className="logoWrapper">
+          <div className="logo"/>
+          <Link href="/">With <span>Zy</span> Productions</Link>
+        </div>
+      </div>
+      <div className="sitemapContainer">
+        <div className="sitemapWrapper">
+          <div className="mainMap">
+            <p>Main Site Map</p>
+            <Link href="/">Main Page</Link>
+            <Link href="/">Portfolio</Link>
+            <Link href="/">Contact</Link>
+            <Link href="/">Rates</Link>
+          </div>
+          <div className="portfolioMap">
+            <p>Portfolio Site Map</p>
+            <Link href="/">Gallery</Link>
+            <Link href="/">Resume</Link>
+            <Link href="/">On Set With Zy</Link>
+            <Link href="/">Shoots With Zy</Link>
+            <Link href="/">Talks With Zy</Link>
+          </div>
+        </div>
       </div>
       <div className="quoteContainer">
-        <p className="quote">"{quote[key]}"</p>
-        <p className="author">- Zyshonne Harris</p>
+        <div className="quoteWrapper">
+          <p className="quote">"{quote[key]}"</p>
+          <p className="author">- Zyshonne Harris</p>
+        </div>
       </div>
       <div className="authorContainer">
-        <div className="">
-
+        <div className="message">
+          <p>WithZyProductions.com</p>
+          <p>Made with care by <Link href="https://vicente0670.com/" title="Vicente0670's website."><span>Vicente0670</span></Link></p>
         </div>
       </div>
       <div className="socialsContainer">
-
+        <Link href="/" id="link" className="instagram"></Link>
+        <Link href="/" id="link" className="spotify"></Link>
+        <Link href="/" id="link" className="email"></Link>
+        <Link href="/" id="link" className="phone"></Link>
       </div>
     </footer>
   )
